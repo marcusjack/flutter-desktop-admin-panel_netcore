22d0087b446280f56b0024bc08c015c68993da4e
isfacademy <isfacademy.2023@gmail.com>
2022-06-09 08:23:32 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:admin/controllers/product_controller.dart';
+import 'package:admin/controllers/studentController.dart';
+import 'package:admin/shared/constants.dart';
+import 'package:admin/controllers/MenuController.dart';
+import 'package:admin/screens/main/main_screen.dart';
+import 'package:admin/shared/remote/dio_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DioHelper.init();
+  //DesktopWindow.setMinWindowSize(Size(1300, 800));
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuController(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => StudentController()..getallStudent(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => ProductController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
+    );
+  }
+}
